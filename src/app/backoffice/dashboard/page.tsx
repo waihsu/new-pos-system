d27827aa93e8a4079c77fe54ff9f814c70335407
waihsu <hsuw647@gmail.com>
2023-09-18 12:11:36 +0630

    feat dashboard

--- src/app/backoffice/dashboard/page.tsx
+++ src/app/backoffice/dashboard/page.tsx
@@ -1,5 +1,54 @@
+import { getLocationId } from "@/app/actions";
+import BackOfficeLayout from "@/components/BackOfficeLayout";
+import { prisma } from "@/lib";
+import { getLCPbySelectedLocationId } from "@/lib/server";
+import { Box, Typography } from "@mui/material";
+import { Card } from "@radix-ui/themes";
+import { format } from "date-fns";
 import React from "react";
 
-export default function page() {
-  return <div>page</div>;
+export default async function Dashboard() {
+  const location_id = await getLocationId();
+  const LCP = await getLCPbySelectedLocationId(location_id);
+
+  const totalOrders = await prisma.orders.count({
+    where: { location_id: location_id },
+  });
+  const todayOrders = await prisma.orders.count({
+    where: { location_id: location_id, createdAt: new Date() },
+  });
+
+  return (
+    <BackOfficeLayout title="Dashboard">
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 3,
+        }}>
+        <Card style={{ padding: "20px 40px" }}>
+          <Typography>Total Products</Typography>
+          <Typography
+            sx={{ textAlign: "center", fontSize: 24, fontWeight: 800 }}>
+            {LCP.length}
+          </Typography>
+        </Card>
+        <Card style={{ padding: "20px 40px" }}>
+          <Typography>Total Orders</Typography>
+          <Typography
+            sx={{ textAlign: "center", fontSize: 24, fontWeight: 800 }}>
+            {totalOrders}
+          </Typography>
+        </Card>
+        <Card style={{ padding: "20px 40px" }}>
+          <Typography>Today Orders</Typography>
+          <Typography
+            sx={{ textAlign: "center", fontSize: 24, fontWeight: 800 }}>
+            {todayOrders}
+          </Typography>
+        </Card>
+      </Box>
+    </BackOfficeLayout>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import ShopCard from "@/components/ShopCard";
 import { Divider, Typography } from "@mui/material";
 import MainNav from "@/components/MainNav";
+import { prisma } from "@/lib";
 
 export default async function Home() {
+  const count = await prisma.locations.count();
   return (
     <div>
       <MainNav />
       <Typography variant="h3" sx={{ textAlign: "center", mt: 16, mb: 3 }}>
         Shop Lists
       </Typography>
-      <ShopCard />
+      <ShopCard count={count} />
       <Divider sx={{ my: 6 }} />
       <h1 style={{ textAlign: "center" }}>Products List</h1>
     </div>
